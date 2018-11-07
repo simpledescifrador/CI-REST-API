ebd0c4e4da52bd1fb9933c930619cac23fca6e8c
Edison T. Villarta <edtvillarta@gmail.com>
2018-11-07 16:04:20 +0800

    add github repo link

--- application/views/admin_dashboard.php
+++ application/views/admin_dashboard.php
@@ -4,9 +4,10 @@
 <div align="center">
   <img src="assets\image\icon.png" alt="app icon" width="50px" height="50px">
   <h2>Welcome to Makahanap Web ADMIN</h2>
+  <p><strong>Github Repo:</strong><a href="https://github.com/simpledescifrador/CI-REST-API" target="_blank">   Click Here Now!</a></p>
 </div>
 <div align="center">
   <h6><em>System is under development</em></h6>
-  <img src="assets\image\loading-sample.gif" alt="Loading System Under Development" width="400px" height="400px">
-  <p>Loading . . .</p>
+  <img src="assets\image\loading-sample.gif" alt="Loading System Under Development" width="300px" height="300px">
+  <h6>Loading . . .</h6>
 </div>
